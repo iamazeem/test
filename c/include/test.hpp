542e71d50af1d49b04d1cba843742abfb9c3421d
Azeem Sajid <azeem.sajid@gmail.com>
2022-06-15 09:25:32 +0500

    Compile sample C program.

---
+++ c/include/test.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <iostream>
+
+void test() noexcept
+{
+    std::cout << "Testing..." << std::endl;
+}
---
+++ c/main.cpp
@@ -0,0 +1,7 @@
+#include "test.hpp"
+
+int main()
+{
+    test();
+    return 0;
+}
