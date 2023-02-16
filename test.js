1c1cf490020e14791995b59461bf02b6d7707f9f
Azeem Sajid <azeem.sajid@gmail.com>
2023-02-16 17:58:10 +0500

    Update codeql_javascript.

--- test.js
+++ test.js
@@ -1,4 +1,12 @@
 const used = true;
 const unused = true;
 
+function test() {
+    var value = 123;
+    var unusedValue = 456;
+    console.log("value:", value);
+}
+
 console.log(used);
+
+test();
