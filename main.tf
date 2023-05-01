0f6aaf88b671937dfc601425af41450db78e86c3
Azeem Sajid <azeem.sajid@gmail.com>
2023-05-01 12:43:06 +0500

    TF Reviewdog Test (#17)

---
+++ main.tf
@@ -0,0 +1,5 @@
+provider "github" {
+    token1 = "ghp_MabcfMTIzZHVtbXl0b2tlbjEyMzEyMzEyMwo="
+    token2 = "ghr_MTIabceIzZHVtbXl0b2tlbjEyMzEyMzEyMwo="
+    token3 = "accf30ed92f4c139e9ea2d255e4c8606fbca2651"
+}
