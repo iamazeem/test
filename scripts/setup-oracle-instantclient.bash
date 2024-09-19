1c5c527bc3fbbcae3776a4976ebd16be9adcfc1a
Azeem Sajid <azeem.sajid@gmail.com>
2024-09-19 12:24:34 +0500

    Update OIC install Bash script

--- scripts/setup-oracle-instantclient.bash
+++ scripts/setup-oracle-instantclient.bash
@@ -4,156 +4,168 @@ set -eo pipefail
 
 # https://www.oracle.com/database/technologies/instant-client/downloads.html
 
-echo "RUNNER_ENVIRONMENT: $RUNNER_ENVIRONMENT"
-echo "RUNNER_OS: $RUNNER_OS"
-echo "RUNNER_ARCH: $RUNNER_ARCH"
+echo "[INF] Installing Oracle Instant Client..."
 
-if [[ $RUNNER_OS == "Linux" && $RUNNER_ARCH == "X64" ]]; then
-    echo "[INF] Installing on Linux..."
+echo "[INF] RUNNER_ENVIRONMENT: $RUNNER_ENVIRONMENT"
+echo "[INF] RUNNER_OS: $RUNNER_OS"
+echo "[INF] RUNNER_ARCH: $RUNNER_ARCH"
 
-    URLS=(
-    https://download.oracle.com/otn_software/linux/instantclient/instantclient-basic-linuxx64.zip
-    https://download.oracle.com/otn_software/linux/instantclient/instantclient-sqlplus-linuxx64.zip
-    )
+if [[ $RUNNER_OS == "Linux" ]]; then
+    echo "[INF] Installing on $RUNNER_OS..."
+
+    URLS=()
+    if [[ $RUNNER_ARCH == "X86" ]]; then
+        URLS=(
+            https://download.oracle.com/otn_software/linux/instantclient/instantclient-basic-linux.zip
+            https://download.oracle.com/otn_software/linux/instantclient/instantclient-sqlplus-linux.zip
+        )
+    elif [[ $RUNNER_ARCH == "X64" ]]; then
+        URLS=(
+            https://download.oracle.com/otn_software/linux/instantclient/instantclient-basic-linuxx64.zip
+            https://download.oracle.com/otn_software/linux/instantclient/instantclient-sqlplus-linuxx64.zip
+        )
+    elif [[ $RUNNER_ARCH == "ARM64" ]]; then
+        URLS=(
+            https://download.oracle.com/otn_software/linux/instantclient/instantclient-basic-linux-arm64.zip
+            https://download.oracle.com/otn_software/linux/instantclient/instantclient-sqlplus-linux-arm64.zip
+        )
+    else
+        echo "[ERR] Unsupported architecture! [$RUNNER_ARCH]"
+        exit 1
+    fi
+
+    INSTALL_BASE_DIR="$RUNNER_TEMP/oracle-instantclient"
+    echo "[INF] INSTALL_BASE_DIR: $INSTALL_BASE_DIR"
+
+    mkdir -p "$INSTALL_BASE_DIR"
+    cd "$INSTALL_BASE_DIR"
 
     for URL in "${URLS[@]}"; do
-    echo "[INF] Downloading... [$URL]"
-    wget -q "$URL"
+        echo "[INF] Downloading... [$URL]"
+        wget -q "$URL"
     done
 
-    INSTALL_BASE_DIR="$GITHUB_WORKSPACE/oracle-instantclient"
+    ls -Gghl ./*.zip
 
     for ZIP in instantclient-*.zip; do
-    unzip -q -o "$ZIP" -d "$INSTALL_BASE_DIR"
+        echo "[INF] Extracting... [$ZIP]"
+        unzip -q -o "$ZIP"
     done
 
     INSTALL_DIR_PATH="$(realpath "$INSTALL_BASE_DIR"/instantclient_*)"
+    echo "[INF] INSTALL_DIR_PATH: $INSTALL_DIR_PATH"
+
     ls -Gghl "$INSTALL_DIR_PATH"
 
     echo "[INF] Setting path... [$INSTALL_DIR_PATH]"
-    echo "$INSTALL_DIR_PATH" >> "$GITHUB_PATH"
+    echo "$INSTALL_DIR_PATH" >>"$GITHUB_PATH"
 
+    echo "[INF] Running ldconfig..."
     echo "$INSTALL_DIR_PATH" | sudo tee /etc/ld.so.conf.d/oracle-instantclient.conf
     sudo ldconfig
 
     echo "[INF] Installed successfully!"
-elif [[ $RUNNER_OS == "macOS" && $RUNNER_ARCH == "X64" ]]; then
-    echo "[INF] Installing on macOS..."
+elif [[ $RUNNER_OS == "macOS" ]]; then
+    echo "[INF] Installing on $RUNNER_OS..."
 
-    URLS=(
-    https://download.oracle.com/otn_software/mac/instantclient/instantclient-basic-macos.zip
-    https://download.oracle.com/otn_software/mac/instantclient/instantclient-sqlplus-macos.zip
-    )
+    URLS=()
+    if [[ $RUNNER_ARCH == "X86" || $RUNNER_ARCH == "X64" ]]; then
+        URLS=(
+            https://download.oracle.com/otn_software/mac/instantclient/instantclient-basic-macos.dmg
+            https://download.oracle.com/otn_software/mac/instantclient/instantclient-sqlplus-macos.dmg
+        )
+    elif [[ $RUNNER_ARCH == "ARM64" ]]; then
+        URLS=(
+            https://download.oracle.com/otn_software/mac/instantclient/instantclient-basic-macos-arm64.dmg
+            https://download.oracle.com/otn_software/mac/instantclient/instantclient-sqlplus-macos-arm64.dmg
+        )
+    else
+        echo "[ERR] Unsupported architecture! [$RUNNER_ARCH]"
+        exit 1
+    fi
 
-    for URL in "${URLS[@]}"; do
-    echo "[INF] Downloading... [$URL]"
-    wget -q "$URL"
-    done
+    INSTALL_BASE_DIR="$RUNNER_TEMP/oracle-instantclient"
+    echo "[INF] INSTALL_BASE_DIR: $INSTALL_BASE_DIR"
 
-    INSTALL_BASE_DIR="$GITHUB_WORKSPACE/oracle-instantclient"
-
-    for ZIP in instantclient-*.zip; do
-    unzip -q -o "$ZIP" -d "$INSTALL_BASE_DIR"
-    done
-
-    INSTALL_DIR_PATH="$(realpath "$INSTALL_BASE_DIR"/instantclient_*)"
-    ls -Gghl "$INSTALL_DIR_PATH"
-
-    echo "[INF] Setting path... [$INSTALL_DIR_PATH]"
-    echo "$INSTALL_DIR_PATH" >> "$GITHUB_PATH"
-
-    echo "[INF] Installed successfully!"
-elif [[ $RUNNER_OS == "macOS" && $RUNNER_ARCH == "ARM64" ]]; then
-    echo "[INF] Installing on macOS..."
-
-    URLS=(
-    https://download.oracle.com/otn_software/mac/instantclient/instantclient-basic-macos-arm64.dmg
-    https://download.oracle.com/otn_software/mac/instantclient/instantclient-sqlplus-macos-arm64.dmg
-    )
+    mkdir -p "$INSTALL_BASE_DIR"
+    cd "$INSTALL_BASE_DIR"
 
     for URL in "${URLS[@]}"; do
-    echo "[INF] Downloading... [$URL]"
-    wget -q "$URL"
+        echo "[INF] Downloading... [$URL]"
+        wget -q "$URL"
     done
 
     ls -Gghl ./*.dmg
 
-    INSTALL_BASE_DIR="$PWD"
-
     for DMG in instantclient-*.dmg; do
+        echo "[INF] Mounting... [$DMG]"
+        hdiutil mount "$DMG"
+        cd /Volumes/instantclient-*
+        echo "[INF] Running install script..."
+        ./install_ic.sh
+        echo "[INF] Unmounting..."
+        hdiutil unmount /Volumes/instantclient-* -force
+    done
+
+    INSTALL_DIR_PATH="$(realpath /Users/"$USER"/Downloads/instantclient_*)"
+    echo "[INF] INSTALL_DIR_PATH: $INSTALL_DIR_PATH"
+
+    ls -Gghl "$INSTALL_DIR_PATH"
+
+    echo "[INF] Setting path... [$INSTALL_DIR_PATH]"
+    echo "$INSTALL_DIR_PATH" >>"$GITHUB_PATH"
+
+    echo "[INF] Installed successfully!"
+elif [[ $RUNNER_OS == "Windows" ]]; then
+    echo "[INF] Installing on $RUNNER_OS..."
+
+    echo "[INF] Installing wget..."
+    choco install wget --no-progress
+
+    URLS=()
+    if [[ $RUNNER_ARCH == "X86" ]]; then
+        URLS=(
+            https://download.oracle.com/otn_software/nt/instantclient/instantclient-basic-nt.zip
+            https://download.oracle.com/otn_software/nt/instantclient/instantclient-sqlplus-nt.zip
+        )
+    elif [[ $RUNNER_ARCH == "X64" ]]; then
+        URLS=(
+            https://download.oracle.com/otn_software/nt/instantclient/instantclient-basic-windows.zip
+            https://download.oracle.com/otn_software/nt/instantclient/instantclient-sqlplus-windows.zip
+        )
+    else
+        echo "[ERR] Unsupported architecture! [$RUNNER_ARCH]"
+        exit 1
+    fi
+
+    INSTALL_BASE_DIR="$RUNNER_TEMP/oracle-instantclient"
+    echo "[INF] INSTALL_BASE_DIR: $INSTALL_BASE_DIR"
+
+    mkdir -p "$INSTALL_BASE_DIR"
     cd "$INSTALL_BASE_DIR"
-    echo "[INF] Mounting... [$DMG]"
-    hdiutil mount "$DMG"
-    sleep 1
-    cd /Volumes/instantclient-*
-    echo "[INF] Running install script..."
-    ./install_ic.sh
-    echo "[INF] Unmounting..."
-    hdiutil unmount /Volumes/instantclient-* -force
-    done
-
-    INSTALL_DIR_PATH="$(realpath /Users/runner/Downloads/instantclient_*)"
-    ls -Gghl "$INSTALL_DIR_PATH"
-
-    echo "[INF] Setting path... [$INSTALL_DIR_PATH]"
-    echo "$INSTALL_DIR_PATH" >> "$GITHUB_PATH"
-
-    echo "[INF] Installed successfully!"
-elif [[ $RUNNER_OS == "Windows" && $RUNNER_ARCH == "X86" ]]; then
-    echo "[INF] Installing on Windows..."
-    choco install wget --no-progress
-
-    URLS=(
-    https://download.oracle.com/otn_software/nt/instantclient/instantclient-basic-nt.zip
-    https://download.oracle.com/otn_software/nt/instantclient/instantclient-sqlplus-nt.zip
-    )
 
     for URL in "${URLS[@]}"; do
-    echo "[INF] Downloading... [$URL]"
-    wget -q "$URL"
+        echo "[INF] Downloading... [$URL]"
+        wget -q "$URL"
     done
 
-    INSTALL_BASE_DIR="$GITHUB_WORKSPACE/oracle-instantclient"
+    ls -Gghl ./*.zip
 
     for ZIP in instantclient-*.zip; do
-    unzip -q -o "$ZIP" -d "$INSTALL_BASE_DIR"
+        echo "[INF] Extracting... [$ZIP]"
+        unzip -q -o "$ZIP"
     done
 
     INSTALL_DIR_PATH="$(realpath "$INSTALL_BASE_DIR"/instantclient_*)"
+    echo "[INF] INSTALL_DIR_PATH: $INSTALL_DIR_PATH"
+
     ls -Gghl "$INSTALL_DIR_PATH"
 
     echo "[INF] Setting path... [$INSTALL_DIR_PATH]"
-    echo "$INSTALL_DIR_PATH" >> "$GITHUB_PATH"
-
-    echo "[INF] Installed successfully!"
-elif [[ $RUNNER_OS == "Windows" && $RUNNER_ARCH == "X64" ]]; then
-    echo "[INF] Installing on Windows..."
-    choco install wget --no-progress
-
-    URLS=(
-    https://download.oracle.com/otn_software/nt/instantclient/instantclient-basic-windows.zip
-    https://download.oracle.com/otn_software/nt/instantclient/instantclient-sqlplus-windows.zip
-    )
-
-    for URL in "${URLS[@]}"; do
-    echo "[INF] Downloading... [$URL]"
-    wget -q "$URL"
-    done
-
-    INSTALL_BASE_DIR="$GITHUB_WORKSPACE/oracle-instantclient"
-
-    for ZIP in instantclient-*.zip; do
-    unzip -q -o "$ZIP" -d "$INSTALL_BASE_DIR"
-    done
-
-    INSTALL_DIR_PATH="$(realpath "$INSTALL_BASE_DIR"/instantclient_*)"
-    ls -Gghl "$INSTALL_DIR_PATH"
-
-    echo "[INF] Setting path... [$INSTALL_DIR_PATH]"
-    echo "$INSTALL_DIR_PATH" >> "$GITHUB_PATH"
+    echo "$INSTALL_DIR_PATH" >>"$GITHUB_PATH"
 
     echo "[INF] Installed successfully!"
 else
-    echo "[ERR] Unsupported OS and/or architecture! [$RUNNER_OS|$RUNNER_ARCH]"
+    echo "[ERR] Unsupported OS! [$RUNNER_OS]"
     exit 1
 fi
