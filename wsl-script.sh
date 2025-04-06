9a04df6ffb3aee00f9a9a22f513cb541403ec5dc
Azeem Sajid <azeem.sajid@gmail.com>
2025-04-06 12:08:34 +0500

    U wsl_env_var

---
+++ wsl-script.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+set -e
+
+echo ">> Running $0"
+echo "WSLENV: $WSLENV"
+echo "TEST_VAR_1: $TEST_VAR_1"
+echo "TEST_VAR_2: $TEST_VAR_2"
+echo "TEST_VAR_3: $TEST_VAR_3"
+echo "--- [DONE] ---"
